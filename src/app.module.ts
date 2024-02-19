0bb165a07792d88722e109c37ef30cf7b052289a
Jose Ricardo Imbaquinga Guaña <99418968+jrimbaquingaguana@users.noreply.github.com>
2024-02-19 14:50:57 -0500

    Deber

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module'; // Importa el módulo de posts
+import { CommentsModule } from './comentarios/comment.module';
+
 
 @Module({
-  imports: [AuthModule, PostsModule], // Importa tanto el módulo de autenticación como el módulo de posts
+  imports: [AuthModule, PostsModule, CommentsModule], // Importa tanto el módulo de autenticación como el módulo de posts
 })
 export class AppModule {}
---
+++ src/comentarios/comment.model.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+  id: string;
+  date: string;
+  content: string;
+  name: string;
+}
